aa383b30376505c6dfda7ce291a7ae49f171a15c
Sagnik Mukhopadhyay <140183632+sagnik1673@users.noreply.github.com>
2024-03-21 22:48:13 +0530

    Create reverseList.java

---
+++ 206. Reverse Linked List/reverseList.java
@@ -0,0 +1,42 @@
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+// Example 1:
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+
+// Example 2:
+// Input: head = [1,2]
+// Output: [2,1]
+
+// Example 3:
+// Input: head = []
+// Output: []
+
+// Constraints:
+// 	The number of nodes in the list is the range [0, 5000].
+// 	-5000 <= Node.val <= 5000
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode nextNode = null;
+        while (curr != null) {
+            nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+}
