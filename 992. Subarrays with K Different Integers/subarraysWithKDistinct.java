9f1ac5169440aff41fda9194163a7eb4bb16c8b9
Sagnik Mukhopadhyay <140183632+sagnik1673@users.noreply.github.com>
2024-03-30 11:03:46 +0530

    Create subarraysWithKDistinct.java

---
+++ 992. Subarrays with K Different Integers/subarraysWithKDistinct.java
@@ -0,0 +1,58 @@
+// Given an integer array nums and an integer k, return the number of good subarrays of nums.
+// A good array is an array where the number of different integers in that array is exactly k.
+// For example, [1,2,3,1,2] has 3 different integers: 1, 2, and 3.
+// A subarray is a contiguous part of an array.
+
+// Example 1:
+// Input: nums = [1,2,1,2,3], k = 2
+// Output: 7
+// Explanation: Subarrays formed with exactly 2 different integers: [1,2], [2,1], [1,2], [2,3], [1,2,1], [2,1,2], [1,2,1,2]
+
+// Example 2:
+// Input: nums = [1,2,1,3,4], k = 3
+// Output: 3
+// Explanation: Subarrays formed with exactly 3 different integers: [1,2,1,3], [2,1,3], [1,3,4].
+
+// Constraints:
+//	1 <= nums.length <= 2 * 10^4
+//	1 <= nums[i], k <= nums.length
+
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        // Array to store the count of distinct values encountered
+        int[] distinctCount = new int[nums.length + 1];
+
+        int totalCount = 0;
+        int left = 0;
+        int right = 0;
+        int currCount = 0;
+
+        while (right < nums.length) {
+            // Increment the count of the current element in the window
+            if (distinctCount[nums[right++]]++ == 0) {
+                // If encountering a new distinct element, decrement K
+                k--;
+            }
+
+            // If K becomes negative, adjust the window from the left
+            if (k < 0) {
+                // Move the left pointer until the count of distinct elements becomes valid again
+                --distinctCount[nums[left++]];
+                k++;
+                currCount = 0;
+            }
+
+            // If K becomes zero, calculate subarrays
+            if (k == 0) {
+                // While the count of left remains greater than 1, keep shrinking the window from the left
+                while (distinctCount[nums[left]] > 1) {
+                    --distinctCount[nums[left++]];
+                    currCount++;
+                }
+                // Add the count of subarrays with K distinct elements to the total count
+                totalCount += (currCount + 1);
+            }
+        }
+        return totalCount;
+    }
+}
